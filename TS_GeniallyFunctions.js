04dedd39c583cda52b8c02da61a83ef6bb35223b
Krissie Sodré <sodre.krissie@gmail.com>
2022-09-26 14:33:54 -0300

    Add files via upload

--- TS_GeniallyFunctions.js
+++ TS_GeniallyFunctions.js
@@ -23,30 +23,12 @@ module.exports = {
 
 */
 
-(function( global, factory ) {
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.html2canvas = factory());
+}(this, (function () { 'use strict';
 
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		// For CommonJS and CommonJS-like environments where a proper window is present,
-		// execute the factory and get jQuery
-		// For environments that do not inherently posses a window with a document
-		// (such as Node.js), expose a jQuery-making factory as module.exports
-		// This accentuates the need for the creation of a real window
-		// e.g. var jQuery = require("jquery")(window);
-		// See ticket #14549 for more info
-		module.exports = global.document ?
-			factory( global, true ) :
-			function( w ) {
-				if ( !w.document ) {
-					throw new Error( "jQuery requires a window with a document" );
-				}
-				return factory( w );
-			};
-	} else {
-		factory( global );
-	}
-
-// Pass this if window is not defined yet
-}(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
 
     /* Códigos iniciam aqui 
 
@@ -65,4 +47,4 @@ function takeScreenshot(){
     console.log("Deu certo!");
 }
 
-}))
+})))
