f325ce96078ad3a9c7f01d5634aa2f3b00d928fb
Krissie Sodré <sodre.krissie@gmail.com>
2022-09-26 15:56:58 -0300

    Add files via upload

--- TS_GeniallyFunctions.js
+++ TS_GeniallyFunctions.js
@@ -1,11 +1,23 @@
 
-/*
+/* Copiar abaixo:
 
+<style>
+.funçao{
+    background-color: #E74C3C;
+    color: #ECF0F1;
+}
+</style>
 <div class="funçao">TeleSapiens_GeniallyFunctions</div><script defer type="text/javascript" src="https://github.com/sodre-krissie/TeleSapiens/blob/main/TS_GeniallyFunctions.js"></script>
 
+//Copiar até aqui e inserir na tela do Genial.ly */
+
+/*
+
+Códigos desenvolvidos pela TeleSapiens
+Em caso de erro, acionar: ksodrE@telesapiens.com.br
 
 */
-
+/*
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
@@ -19,7 +31,9 @@ exports.handler = async (event) => {
     return response;
 };
 
-/* Códigos iniciam aqui 
+*/
+
+/* Códigos iniciam aqui */
 
 var html2canvas = document.createElement('script');
 html2canvas.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
@@ -29,9 +43,12 @@ var FileSaver = document.createElement('script');
 FileSaver.src = "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js";
 document.head.appendChild(FileSaver);
 
-*/
 
-document.getElementById("botao2").onclick = function takeScreenshot(){
 
+function takeScreenshot(){
     console.log("Deu certo!");
+}
+
+document.getElementsByTagName("botao2").onclick = function(){
+    console.log("Deu errado?");
 }
