c3922c1b95e673504f221bfddd90e61fdd3e0628
Krissie Sodré <sodre.krissie@gmail.com>
2022-09-26 14:29:56 -0300

    Add files via upload

--- TS_GeniallyFunctions.js
+++ TS_GeniallyFunctions.js
@@ -3,7 +3,7 @@
 
 <div class="funçao">TeleSapiens_GeniallyFunctions</div><script defer type="text/javascript" src="https://github.com/sodre-krissie/TeleSapiens/blob/main/TS_GeniallyFunctions.js"></script>
 
-*/
+
 
 module.exports = {
     async Headers(){
@@ -21,7 +21,34 @@ module.exports = {
     }
 }
 
+*/
 
+(function( global, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		// For CommonJS and CommonJS-like environments where a proper window is present,
+		// execute the factory and get jQuery
+		// For environments that do not inherently posses a window with a document
+		// (such as Node.js), expose a jQuery-making factory as module.exports
+		// This accentuates the need for the creation of a real window
+		// e.g. var jQuery = require("jquery")(window);
+		// See ticket #14549 for more info
+		module.exports = global.document ?
+			factory( global, true ) :
+			function( w ) {
+				if ( !w.document ) {
+					throw new Error( "jQuery requires a window with a document" );
+				}
+				return factory( w );
+			};
+	} else {
+		factory( global );
+	}
+
+// Pass this if window is not defined yet
+}(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+
+    /* Códigos iniciam aqui 
 
 var html2canvas = document.createElement('script');
 html2canvas.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
@@ -31,8 +58,11 @@ var FileSaver = document.createElement('script');
 FileSaver.src = "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js";
 document.head.appendChild(FileSaver);
 
+*/
 
 function takeScreenshot(){
 
     console.log("Deu certo!");
 }
+
+}))
