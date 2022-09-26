9a9caca27b92a529a774c6398448d9392ce01a9c
Krissie Sodré <sodre.krissie@gmail.com>
2022-09-26 10:59:21 -0300

    Add files via upload

--- TS_GeniallyFunctions.js
+++ TS_GeniallyFunctions.js
@@ -1,9 +1,27 @@
 
 /*
 
-<div class="funçao">TeleSapiens_GeniallyFunctions</div><script src="https://github.com/sodre-krissie/TeleSapiens/blob/main/TS_GeniallyFunctions.js" async></script>
+<div class="funçao">TeleSapiens_GeniallyFunctions</div><script defer type="text/javascript" src="https://github.com/sodre-krissie/TeleSapiens/blob/main/TS_GeniallyFunctions.js"></script>
 
 */
+
+module.exports = {
+    async Headers(){
+        return[
+            {
+                source: '/:path*',
+                Headers: [
+                    {key: 'Access-Control-Allow-Credentials', value: 'true'},
+                    {key: 'Access-Control-Allow-Origins', value: '*'},
+                    {key: 'Access-Control-Allow-Origins', value: 'POST, GET, PUT, OPTIONS, DELETE, PATCH'},
+                    {key: 'Access-Control-Allow-Headers', value: 'Content-Type'},
+                ]
+            }
+        ]
+    }
+}
+
+
 
 var html2canvas = document.createElement('script');
 html2canvas.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
