581a40e6263ad35efb7bbad7e52201e181bf22c6
KSodre <sodre.krissie@gmail.com>
2022-12-07 14:05:29 -0300

    v0.4

--- LigarPontos.js
+++ LigarPontos.js
@@ -1,3 +1,32 @@
+/*
+
+<link rel="stylesheet" href="https://bit.ly/3f2nSUc">
+<div class="funçao">TeleSapiens_LigarPontos</div>
+<script defer type="application/javascript" src="https://msbarquitetura.com.br/LigarPontos.js" crossorigin="anonymous"></script>
+
+*/
+
+    // var _canvas = document.getElementById("meucanvas");
+    // var ctx = _canvas.getContext("2d");
+
+    // let WIDTH = document.documentElement.clientWidth;
+    // let HEIGHT = document.documentElement.clientHeight;
+
+    // _canvas.width = WIDTH;
+    // _canvas.height = HEIGHT;
+
+    // for (var x=0; x<=WIDTH; x=x+50){
+    // ctx.moveTo(x,0);
+    // ctx.lineTo(x,HEIGHT);
+    // }
+
+    // for (var y=0; y<=HEIGHT; y=y+50){
+    // ctx.moveTo(0,y);
+    // ctx.lineTo(WIDTH,y);
+    // }
+    // ctx.closePath();
+    // ctx.strokeStyle = "#4FC3F7";
+    // ctx.stroke();
 
 // let alternativasLista = ['A1', 'A2', 'A3', 'A4'];
 // let respostasLista = ['R1', 'R2', 'R3', 'R4'];
@@ -5,8 +34,28 @@
 let alternativasPontos = false;
 let respostasPontos = false;
 
+let espessuraLinha = 10;
+
+// let combinacoesCertas = [
+//     ['A1','R1'],
+// ];
+
+// function verificaQuestao(respostaCorreta){
+
+//     for (pos of combinacoesCertas){
+//         if (pos.every((item)=> item.includes(item))){
+//             alert("certo");
+//         }
+//     }
+    
+    
+//     console.warn("Resposta correta: "+ respostaCorreta);
+//     // console.log(alternativaClicada);
+        
+// }
+
 function check(){
-    console.warn("Checado");
+    console.log("Checagem");
     ctx.beginPath();
     ctx.closePath();
 
@@ -27,18 +76,21 @@ function pontoAlternativa(idPonto){
         console.log("Resposta clicada");
         ctx.lineTo(x,y);
         ctx.closePath();
-        ctx.lineWidth = 10;
+        ctx.lineWidth = espessuraLinha;
         ctx.strokeStyle = "#E65100";
         ctx.stroke(); 
-        console.log("Linha finalizada");
+        console.warn("Linha finalizada");
         return check();
     }else{
-        console.warn("Resposta NAO clicada");
+        console.log("Resposta NAO clicada");
         ctx.beginPath(); 
         ctx.moveTo(x,y);
     }
 
-    let alternativaClicada = idPonto.toString();
+    let alternativaClicada = idPonto;
+    // let alternativaClicada = idPonto.toString();
+    // let alternativaClicada = idPonto.target.getAttribute("data-i");
+    console.log(alternativaClicada);
 
     alternativasPontos = true;
     return alternativasPontos, alternativaClicada;
@@ -55,13 +107,13 @@ function pontoResposta(idPonto){
         console.log("Alternativa clicada");
         ctx.lineTo(x,y);
         ctx.closePath();
-        ctx.lineWidth = 10;
+        ctx.lineWidth = espessuraLinha;
         ctx.strokeStyle = "#E65100";
         ctx.stroke();
-        console.log("Linha finalizada");
+        console.warn("Linha finalizada");
         return check();
     }else{
-        console.warn("Alternativa NAO clicada");
+        console.log("Alternativa NAO clicada");
         ctx.beginPath(); 
         ctx.moveTo(x,y);
     }
@@ -70,8 +122,12 @@ function pontoResposta(idPonto){
     return respostasPontos;
 }
 
-function verificaQuestao(respostaCorreta){
-    console.warn("Resposta correta: "+ respostaCorreta);
-    // console.log(alternativaClicada);
-        
-}
+// function verificaQuestao(respostaCorreta){
+// console.log(respostaCorreta);
+//     if(alternativaClicada==="A1"){
+//         console.log('Você acertou');
+//     }else{
+//         return;
+//     }
+
+// }
